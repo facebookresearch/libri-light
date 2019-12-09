aae396eb0470fe466affbb8a18865a2e24cd574c
Molugan <mriviere@fb.com>
2019-12-09 16:14:35 +0100

    BUGFIX + CLEANING: build_all_stats (#8)

--- data_preparation/build_all_stats.py
+++ data_preparation/build_all_stats.py
@@ -5,12 +5,9 @@ import plot
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
-        description="Build the statistics on LibriBig")
-    parser.add_argument('path_metadata', type=str,
-                        help="Path to the directory containing the metadata")
-    parser.add_argument('path_wav', type=str,
-                        help="Path to the directory containing the wav data")
+    parser = argparse.ArgumentParser(description="Build the statistics on LibriBig")
+    parser.add_argument('path_data', type=str,
+                        help="Path to the directory containing the data")
     parser.add_argument('out_dir', type=str,
                         help="Path to the output directory")
     parser.add_argument('--ignore_cache', action='store_true')
@@ -29,16 +26,17 @@ if __name__ == "__main__":
     path_cache_metadata = path_cache / "metadata.pkl"
     list_metadata = ut.load_cache(path_cache_metadata,
                                   ut.get_all_metadata,
-                                  args=(args.path_metadata,),
+                                  args=(args.path_data, ".json"),
                                   ignore_cache=args.ignore_cache)
+    print(f"{len(list_metadata)} files found")
 
     # Get the genre statistics
     print("Building the genre statistics")
     path_genre_stats = path_cache / "meta_genre_stats.json"
     genre_data = ut.load_cache(path_genre_stats,
                                ut.get_hour_tag_repartition,
-                               args=(args.path_metadata, list_metadata,
-                                     "meta_genre", args.path_wav),
+                               args=(args.path_data, list_metadata,
+                                     "meta_genre", ".flac"),
                                ignore_cache=args.ignore_cache)
 
     path_tags_hist = args.out_dir / "meta_genres.png"
@@ -52,8 +50,8 @@ if __name__ == "__main__":
     path_speaker_cache = path_cache / "speaker_stats.json"
     speaker_data = ut.load_cache(path_speaker_cache,
                                  ut.get_speaker_hours_data,
-                                 args=(args.path_metadata, list_metadata,
-                                       args.path_wav))
+                                 args=(args.path_data, list_metadata,
+                                       ".flac"))
 
     speaker_hours = [x for _, x in speaker_data.items()]
     path_speaker_hist = args.out_dir / "speaker_data.png"
