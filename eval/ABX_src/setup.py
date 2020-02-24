3e7005bd556a46085889df3b18ed02386c19c9d1
Molugan <mriviere@fb.com>
2020-02-24 09:45:19 -0800

    BUGFIX: numpy include

--- eval/ABX_src/setup.py
+++ eval/ABX_src/setup.py
@@ -1,7 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
+    include_dirs=[numpy.get_include()],
     ext_modules=cythonize("dtw.pyx")
 )
